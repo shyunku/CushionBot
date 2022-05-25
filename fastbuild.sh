6291225c4d0281de8c864b5d3f30a4523cef50e6
shyunku <whdudgns7321@gmail.com>
2022-05-25 20:44:16 +0900

    update build.gradle

--- fastbuild.sh
+++ fastbuild.sh
@@ -1,5 +1,5 @@
 echo "============ BUILD START ============"
-rm build/libs/CushionBot-1.0-SNAPSHOT.jar
+rm -rf build/libs
 ./gradlew clean build
 java -jar build/libs/CushionBot-1.0-SNAPSHOT.jar
 echo "============ BUILD END ============"
