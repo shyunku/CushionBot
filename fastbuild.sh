58ce03ec5c399ac1aa5c8229310384a445b1d498
shyunku <whdudgns7321@gmail.com>
2022-05-25 22:38:13 +0900

    add nohup

--- fastbuild.sh
+++ fastbuild.sh
@@ -1,5 +1,5 @@
 echo "============ BUILD START ============"
 rm -rf build/libs
 ./gradlew clean build
-java -jar build/libs/CushionBot-1.0-SNAPSHOT.jar
+nohup java -jar build/libs/CushionBot-1.0-SNAPSHOT.jar &
 echo "============ BUILD END ============"
