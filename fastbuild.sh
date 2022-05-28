ee0d473a7dc6a92f104c015f03ce70b40b51c1ce
shyunku <whdudgns7321@gmail.com>
2022-05-28 20:22:47 +0900

    update fastbuild

--- fastbuild.sh
+++ fastbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "============ BUILD START ============"
-sh stop.sh
+sudo sh stop.sh
 rm -rf build/libs
 ./gradlew clean build
-sh start.sh
+sudo sh start.sh
 echo "============ BUILD END ============"
