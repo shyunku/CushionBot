96a2830ef17454b1d777242b231aac2d900843b4
shyunku <whdudgns7321@gmail.com>
2023-07-19 02:40:23 +0900

    fix message event fail error

--- src/main/java/core/CushionBot.java
+++ src/main/java/core/CushionBot.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
 
@@ -16,6 +17,8 @@ public class CushionBot {
 
     public static void main(String[] args) throws LoginException {
         jda = JDABuilder.createDefault(BOT_TOKEN)
+                .enableIntents(GatewayIntent.GUILD_MESSAGES)
+                .enableIntents(GatewayIntent.DIRECT_MESSAGES)
                 .addEventListeners(new InternalEventListener())
                 .build();
     }
