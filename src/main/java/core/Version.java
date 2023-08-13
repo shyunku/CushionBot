36c74389e3ebee8f380173f1d7e40109b71a8b29
shyunku <whdudgns7321@gmail.com>
2023-08-14 03:07:54 +0900

    update version

--- src/main/java/core/Version.java
+++ src/main/java/core/Version.java
@@ -3,6 +3,6 @@ package core;
 import Utilities.TokenManager;
 
 public class Version {
-    public static String CURRENT = "2.1.0.1";
+    public static String CURRENT = "2.1.0.3";
     public static boolean PRODUCTION_MODE = new TokenManager().isProduction();
 }
