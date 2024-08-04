69a87419a6c200f9fc2ca180704f6d7a656180d8
shyunku <whdudgns7321@gmail.com>
2024-08-05 01:43:48 +0900

    reorder user for watcher

--- src/main/java/core/Version.java
+++ src/main/java/core/Version.java
@@ -3,6 +3,6 @@ package core;
 import Utilities.TokenManager;
 
 public class Version {
-    public static String CURRENT = "2.2.6.0";
+    public static String CURRENT = "2.2.6.1";
     public static boolean PRODUCTION_MODE = new TokenManager().isProduction();
 }
