2ff8f392a69953410039b35a6a2dd8ee7c45f563
shyunku <whdudgns7321@gmail.com>
2024-08-04 21:30:12 +0900

    fix command

--- src/main/java/core/Version.java
+++ src/main/java/core/Version.java
@@ -3,6 +3,6 @@ package core;
 import Utilities.TokenManager;
 
 public class Version {
-    public static String CURRENT = "2.2.5.1";
+    public static String CURRENT = "2.2.5.2";
     public static boolean PRODUCTION_MODE = new TokenManager().isProduction();
 }
