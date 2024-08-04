e4e8774715b633327017e04a8bf482f6b145b1eb
shyunku <whdudgns7321@gmail.com>
2024-08-05 01:32:23 +0900

    fix error

--- src/main/java/core/Version.java
+++ src/main/java/core/Version.java
@@ -3,6 +3,6 @@ package core;
 import Utilities.TokenManager;
 
 public class Version {
-    public static String CURRENT = "2.2.5.4";
+    public static String CURRENT = "2.2.5.5";
     public static boolean PRODUCTION_MODE = new TokenManager().isProduction();
 }
