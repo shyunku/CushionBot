1eebaf10dbf030eca1530f77dce50660f27b060d
shyunku <whdudgns7321@gmail.com>
2024-08-04 21:30:12 +0900

    fix command

--- src/main/java/core/command/SlashCommandParser.java
+++ src/main/java/core/command/SlashCommandParser.java
@@ -95,7 +95,7 @@ public class SlashCommandParser {
                 String durationStr = Util.getDurationString(totalDuration);
 //                Member member = guild.retrieveMemberById(userId).complete();
 //                String memberName = member == null ? "알 수 없음" : member.getEffectiveName();
-                embedBuilder.addField(String.format("[%d위]", i + 1), TextStyler.Block(String.format("<@%s> %s", userId, durationStr)), false);
+                embedBuilder.addField(String.format("[%d위]", i + 1), String.format("<@%s> %s", userId, TextStyler.Block(durationStr)), false);
             }
 
             MessageEmbed embed = embedBuilder.build();
