f547892e64456af3560c13efd5addfa027bd3dd8
shyunku <whdudgns7321@gmail.com>
2024-08-06 15:44:49 +0900

    add close connection when ping fail

--- src/main/java/core/Version.java
+++ src/main/java/core/Version.java
@@ -3,6 +3,6 @@ package core;
 import Utilities.TokenManager;
 
 public class Version {
-    public static String CURRENT = "2.2.7.7";
+    public static String CURRENT = "2.2.7.8";
     public static boolean PRODUCTION_MODE = new TokenManager().isProduction();
 }
