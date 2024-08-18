9cd30424996ad99200fa58a1c0cf3abde3c461b3
shyunku <whdudgns7321@gmail.com>
2024-08-18 19:49:55 +0900

    update stop.sh to graceful stop

--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ps -ef | grep CushionBot | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep CushionBot | grep -v grep | awk '{print $2}' | xargs kill -15
 echo "============ DAEMON STOPPED ============"
