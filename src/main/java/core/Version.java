08e849ddbaba9fc1c7352c6054bf97e9f953435e
shyunku <whdudgns7321@gmail.com>
2024-08-18 14:07:41 +0900

    add apache pool2

--- src/main/java/core/Version.java
+++ src/main/java/core/Version.java
@@ -3,6 +3,6 @@ package core;
 import Utilities.TokenManager;
 
 public class Version {
-    public static String CURRENT = "2.3.0.0";
+    public static String CURRENT = "2.3.0.1";
     public static boolean PRODUCTION_MODE = new TokenManager().isProduction();
 }
