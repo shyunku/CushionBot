4f3302624598efd0639c5191a5e6f6cae6d2802c
shyunku <whdudgns7321@gmail.com>
2024-09-28 04:36:42 +0900

    update lavalink & pot tken & search token

--- src/main/java/core/Version.java
+++ src/main/java/core/Version.java
@@ -3,6 +3,6 @@ package core;
 import Utilities.TokenManager;
 
 public class Version {
-    public static String CURRENT = "3.2.5.0";
+    public static String CURRENT = "3.2.5.1";
     public static boolean PRODUCTION_MODE = new TokenManager().isProduction();
 }
